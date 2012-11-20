e9d150176acf73cd29704e80ed63334ea5fa4ff4
Paweł Sołtysiak <soltys@uznam.org>
2012-11-20 20:13:50 +0100

    init commit

---
+++ src/wsi/puzzleN2_1/PuzzleSolver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsi.puzzleN2_1;
+
+/**
+ *
+ * @author Soltys
+ */
+public class PuzzleSolver
+{
+
+}
---
+++ src/wsi/puzzleN2_1/Puzzle_n2_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsi.puzzleN2_1;
+
+/**
+ *
+ * @author Soltys
+ */
+public class Puzzle_n2_1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
